426fa8da67853d4662631c8924a5fb03f88f5728
Adhy Wiranata <adhy.prasetyo@tiket.com>
2021-10-23 06:35:12 +0700

    up

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>I Am Queued</h1>
+        <h1 className={styles.title}>I Am NOT Queued</h1>
       </main>
       <RenderThem />
     </div>
